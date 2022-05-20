53eb7e94a0b079c060c890d2382f0333584d8047
Dimty <nikitin-dmitriyy@mail.ru>
2022-05-20 15:57:30 +0600

    Add function "Sum"

--- Git_praktika/Program.cs
+++ Git_praktika/Program.cs
@@ -4,7 +4,18 @@
     {
         public static void Main(string[] args)
         {
-                        
+            
+        }
+
+        static void Sum(int[] arr)
+        {
+            var sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
